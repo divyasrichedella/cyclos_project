82c934908c80566db1c4b930a446d54733256fce
divyasrichedella <60428425+divyasrichedella@users.noreply.github.com>
2020-02-18 14:03:15 +0530

    Divya loginPOM
    testcase loginPOM

--- CyclosProject/src/com/training/pom/LoginPOM.java
+++ CyclosProject/src/com/training/pom/LoginPOM.java
@@ -2,11 +2,11 @@ package com.training.pom;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 import org.testng.Assert;
 
 public class LoginPOM {
@@ -18,52 +18,61 @@ public class LoginPOM {
 	}
 	
 	@FindBy(id="cyclosUsername")
-	private WebElement userName; 
+	private WebElement sendUserName; 
 	
+	@FindBy(id="cyclosPassword")
+	private WebElement password;
 	
 	@FindBy(xpath="//input[@value='1']")
 	private WebElement password1;
+	
 	@FindBy(xpath="//input[@value='2']")
 	private WebElement password2;
+	
 	@FindBy(xpath="//input[@value='3']")
 	private WebElement password3;
+	
 	@FindBy(xpath="//input[@value='4']")
 	private WebElement password4;
+
 	@FindBy(xpath="//input[@value='Submit']")
-	private WebElement loginBtn; 
-	@FindBy(id="memberUsername")
-	private WebElement membername; 
-	@FindBy(xpath="//input[@linkurl='grantLoan?memberId=12']")
-	private WebElement grant;
-	@FindBy(id="amount")
-	private WebElement amount;
-	@FindBy(id="description")
-	private WebElement des;
-	@FindBy(xpath="//input[@type='submit']")
-	private WebElement sub;
-	@FindBy(xpath="//input[@type='submit']")
-	private WebElement sub1;
-	@FindBy(xpath="//input[@linkurl='searchLoans?memberId=12']")
-	private WebElement view;
-	@FindBy(id=("menu15"))
-	private WebElement view1;
-	@FindBy(xpath="//li[@id='menu2']")
-	private WebElement men;
-	@FindBy(id="submenu2.3")
-	private WebElement men1; 
+	private WebElement loginBtn;
+	
+	@FindBy(xpath="//li[@id='menu1']")
+	private WebElement personalBtn;
+	
+	@FindBy(xpath="//li[@id='submenu1.0']")
+	private WebElement profileBtn;
+	
+	@FindBy(id="modifyButton")
+	private WebElement changeBtn;
+	
+	@FindBy(xpath="//input[@value='Administrator']")
+	private WebElement cleartxtBox;
+	
+	@FindBy(xpath="//input[@value='Administrator']")
+	private WebElement sendAdministratorName;
+	
+	@FindBy(id="saveButton")
+	private WebElement saveButton;
+	
 	public void sendUserName(String userName) {
-		this.userName.clear();
-		this.userName.sendKeys(userName);
+		this.sendUserName.clear();
+		this.sendUserName.sendKeys(userName);
 	}
+	
 	public void password1(){
 		this.password1.click();
 	}
+	
 	public void password2(){
 		this.password2.click();
 	}
+	
 	public void password3(){
 		this.password3.click();
 	}
+	
 	public void password4(){
 		this.password4.click();
 	}
@@ -71,62 +80,45 @@ public class LoginPOM {
 	public void clickLoginBtn() {
 		this.loginBtn.click(); 
 	}
-	public void membername(String membername) {
-		this.membername.clear();
-		this.membername.sendKeys(membername);
-	}
-	public void exe() throws InterruptedException{
-		JavascriptExecutor a = (JavascriptExecutor)driver;
 
-	    a.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-	    Thread.sleep(3000);
+	public void personalBtn() {
+		this.personalBtn.click();
+		
+	}
 
+	public void profileBtn() {
+		this.profileBtn.click();
+		
 	}
-	public void grant(){
-		this.grant.click();
+
+	public void changeBtn() {
+		this.changeBtn.click();
+		
 	}
-	public void amount(String amount) {
-		this.amount.clear();
-		this.amount.sendKeys(amount);
+
+	
+	public void cleartxtBox() {
+		this.cleartxtBox.clear();
+		
 	}
-	public void des(String des) {
-		this.des.clear();
-		this.des.sendKeys(des);
+
+	public void sendAdministratorName(String string) {
+		this.sendAdministratorName.sendKeys(string);
+		
 	}
-	public void sub(){
-		this.sub.click();
-	}
-	public void sub1(){
-		this.sub1.click();
-	}
-	public void alertHandle1(){
-		Alert d=driver.switchTo().alert();
-		String expected="The loan was successfully granted";
-		//String actual=driver.findElement(By.xpath("//td[@align='center']")).getText();
-		String actual=d.getText();
-		Assert.assertEquals(expected,actual);
-		d.accept();
-	}
-	public void view(){
-		this.view.click();
-	}
-	public void view1(){
-		this.view1.click();
-	}
-	public void men(){
-		this.men.click();
-	}
-	public void men1(){
-		this.men1.click();
-	}
-	public void alertHandle2(){
-		Alert d=driver.switchTo().alert();
-		String expected="Please confirm to logout";
-		//String actual=driver.findElement(By.xpath("//td[@align='center']")).getText();
-		String actual=d.getText();
-		Assert.assertEquals(expected,actual);
-		d.accept();
+
+	public void saveButton() {
+		this.saveButton.click();
+		
 	}
 	
+	public void alertHandle() {
+	Alert alt=driver.switchTo().alert();
+	String expected="Profile modified";
+	String actual=alt.getText();
+	Assert.assertEquals(expected, actual);
+	alt.accept();
+
+	}	
 	
 }
